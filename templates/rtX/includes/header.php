35882022b0d5f04868dfcec4efbc2539ac8f77c2
Tia <zencilererer123@gmail.com>
2022-08-21 15:58:33 +0300

    some fixes for ui
    The sidebar attempt failed and has been removed ✔👌
    
    Bootstrap UI will be made (as I don't like making UI) ✔👌

--- templates/rtX/includes/header.php
+++ templates/rtX/includes/header.php
@@ -11,6 +11,8 @@ global $rtX;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -22,29 +24,40 @@ global $rtX;
 </head>
 
 <body>
-	<div id="colorlib-page">
-		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-		<aside id="colorlib-aside" role="complementary" class="js-fullheight">
-			<nav id="colorlib-main-menu" role="navigation">
-				<ul>
-					<img class="avatar" src="<?= $rtX['rtXLookPath'] . User::userData('look') ?>&head_direction=3&direction=2&action=wlk&gesture=sml" alt="<?= User::userData('username') ?>'s Avatar">
-					<li class="colorlib-active"><a href="#">
-							<?= User::userData('username') ?>
-						</a></li>
-					<li><a href="/community">Community</a></li>
-					<li><a href="/staff">Staffs</a></li>
-					<li><a href="/news">News</a></li>
-					<li><a href="#" style="color:gray;">Settings</a></li>
-					<li><a href="#" style="color:red;">Log out</a></li>
+	<nav class="navbar navbar-expand-lg navbar-dark">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#"><?= User::userData('username') ?></a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Community
+						</a>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="/news/">News</a></li>
+							<li><a class="dropdown-item" href="/staff">Staffs</a></li>
+							<li>
+								<hr class="dropdown-divider">
+							</li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled">Disabled</a>
+					</li>
 				</ul>
-			</nav>
-		</aside> <!-- END COLORLIB-ASIDE -->
-	</div><!-- END COLORLIB-PAGE -->
-
-	<script src="/templates/<?= $config['skin'] ?>/js/jquery.min.js"></script>
-	<script src="/templates/<?= $config['skin'] ?>/js/popper.js"></script>
-	<script src="/templates/<?= $config['skin'] ?>/js/bootstrap.min.js"></script>
-	<script src="/templates/<?= $config['skin'] ?>/js/main.js"></script>
+				<form class="d-flex" role="search">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	</nav>
 </body>
 
 </html>
--- templates/rtX/me.php
+++ templates/rtX/me.php
@@ -2,14 +2,3 @@
 $page = 'Anasayfa';
 require $rtX['rtXPath'] . 'includes/header.php';
 ?>
-<div id="colorlib-main">
-    <section class="ftco-section pt-4 mb-5 ftco-intro">
-        <div class="container-fluid px-3 px-md-0">
-            <div class="row">
-                <div class="user-box col-xl-7 col-lg-7 col-md-7 col-sm-12 col-12 mb-4">
-                    test
-                </div>
-            </div>
-        </div>
-    </section>
-</div><!-- END COLORLIB-MAIN -->
